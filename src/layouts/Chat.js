6d6e3f495d500e57cf5b46d41d8bc617e07b13fa
Tisha <nusratjahantisha763@gmail.com>
2022-06-17 18:34:01 +0600

    test

--- src/layouts/Chat.js
+++ src/layouts/Chat.js
@@ -1,6 +1,7 @@
 import ChatContent from '../components/Chat/ChatContent'
 
 function Chat() {
+    console.log("2")
     return (
         <div style={{ background: "#fff" }}>
             <div style={{ background: '#f4f3f8', display: 'flex' }}>
